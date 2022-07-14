725d5e50029b38dcfc837e8f2d8f4d3bf2700978
feg59crz <fernandogomescruz@gmail.com>
2022-07-14 12:11:17 -0300

    README

---
+++ REAME.md
@@ -0,0 +1,3 @@
+# Guessing Game
+My version of the "guessing game".
+
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
   println!("=== Guessing Game! ===\n");
   
   let secret_number = rand::thread_rng().gen_range(1..=100);
-  let mut attempts: u8 = 10;
+  let mut attempts: u8 = 5;
 
 loop {
   if attempts == 0 {
@@ -23,16 +23,34 @@ loop {
   let guess: u32 = match guess.trim().parse() {
     Ok(num) => {
       if num > 100 || num < 1 {
-        println!("Please! Type a number between 1 and 100!");
+        println!("Please! Type a number between 1 and 100!\n");
         continue;
       }
       num  
     },
     Err(_) => {
-      println!("Please! Type a number!");
+      println!("Please! Type a number!\n");
       continue;
     }
   };
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => {
+      println!("Too small!\n");
+      attempts -= 1;
+    },
+
+    Ordering::Greater => {
+      println!("Too big!\n");
+      attempts -= 1;
+    },
+
+    Ordering::Equal => {
+      println!("You Win!");
+      break;
+    }
+
+ }
    
 
 }
